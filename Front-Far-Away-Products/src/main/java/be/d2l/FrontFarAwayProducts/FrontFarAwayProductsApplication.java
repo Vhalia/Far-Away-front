dcd51593ac6656c2415707dc414446b7a8f4ada0
ThomasLempereur <tho.lempereur@gmail.com>
2021-11-27 13:46:46 +0100

    initial commit

---
+++ Front-Far-Away-Products/src/main/java/be/d2l/FrontFarAwayProducts/FrontFarAwayProductsApplication.java
@@ -0,0 +1,13 @@
+package be.d2l.FrontFarAwayProducts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrontFarAwayProductsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrontFarAwayProductsApplication.class, args);
+	}
+
+}
---
+++ Front-Far-Away-Products/src/test/java/be/d2l/FrontFarAwayProducts/FrontFarAwayProductsApplicationTests.java
@@ -0,0 +1,13 @@
+package be.d2l.FrontFarAwayProducts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrontFarAwayProductsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
