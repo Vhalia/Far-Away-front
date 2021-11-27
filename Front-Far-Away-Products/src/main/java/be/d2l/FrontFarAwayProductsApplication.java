feddaf3cdb7848d3b2443f5dd557e7adb865bd19
ThomasLempereur <tho.lempereur@gmail.com>
2021-11-27 13:57:12 +0100

    add - main and model

--- Front-Far-Away-Products/src/main/java/be/d2l/FrontFarAwayProductsApplication.java
+++ Front-Far-Away-Products/src/main/java/be/d2l/FrontFarAwayProductsApplication.java
@@ -1,9 +1,13 @@
-package be.d2l.FrontFarAwayProducts;
+package be.d2l;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
+@EnableEurekaClient
 public class FrontFarAwayProductsApplication {
 
 	public static void main(String[] args) {
---
+++ Front-Far-Away-Products/src/main/java/be/d2l/model/Product.java
@@ -0,0 +1,17 @@
+package be.d2l.model;
+
+import lombok.Data;
+
+@Data
+public class Product {
+
+    private  int id;
+    private String name;
+    private String shortDescription;
+    private String detailedDescription;
+    private Float price;
+    private String category;
+
+    public Product(){
+    }
+}
